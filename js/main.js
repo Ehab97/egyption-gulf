d5aa125b9a412c80bd90b42755a75b6ac86c342c
Ehab97 <ehabreda04@gmail.com>
2019-06-22 22:12:07 +0200

    first push

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//document.addEventListener("DOMContentLoaded",func(){};
+//$(document).ready(func(){});
+document.addEventListener("DOMContentLoaded", function() {
+
+
+});
+
+new Vue({
+    el: '#app',
+    data: {},
+    methods: {
+
+    },
+
+});
