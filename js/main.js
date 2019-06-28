3473df18415a7f23ec6df326de44815cee99be58
Ehab97 <ehabreda04@gmail.com>
2019-06-28 22:46:47 +0200

    contacts money

--- js/main.js
+++ js/main.js
@@ -1,13 +1,35 @@
 //document.addEventListener("DOMContentLoaded",func(){};
 //$(document).ready(func(){});
 document.addEventListener("DOMContentLoaded", function() {
-    const mq = window.matchMedia("(min-width: 500px)");
+    // const mq = window.matchMedia("(min-width: 500px)");
+    var tabColor = document.querySelectorAll('.navigate ul li');
+    var ChangeTapColor = function() {
+        for (let i = 0; i < tabColor.length; ++i) {
+            tabColor[i].onclick = () => {
+                var c = 0;
+                while (c < tabColor.length) {
+                    // tabColor[c].classList.contains('activeTap') ? tabColor.classList.remove('activeTap') : '';
+                    if (tabColor[c].classList.contains('liActive')) {
+                        tabColor[c].classList.remove('liActive');
+                        tabColor[c].style.color = '';
+                    }
+                    c++;
+                }
+                tabColor[i].classList.add('liActive');
+                tabColor[i].style.color = '#2ED47A';
+            }
+        }
+    }
+    ChangeTapColor();
 
 });
-
 new Vue({
     el: '#app',
-    data: {},
+    data: {
+        benfits: true,
+        paid: false,
+        totalBenfits: false
+    },
     methods: {
 
     },
