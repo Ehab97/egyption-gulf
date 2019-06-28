759e14d1b85d0ce0557c99974f4e4b1ec4b16c52
Ehab97 <ehabreda04@gmail.com>
2019-06-28 07:55:53 +0200

    home and renters

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //document.addEventListener("DOMContentLoaded",func(){};
 //$(document).ready(func(){});
 document.addEventListener("DOMContentLoaded", function() {
-
+    const mq = window.matchMedia("(min-width: 500px)");
 
 });
 
