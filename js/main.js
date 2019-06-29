2b3925c98c38f0d0398545ed01045649ae28b0c7
Ehab97 <ehabreda04@gmail.com>
2019-06-29 21:42:25 +0200

    renter update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-//document.addEventListener("DOMContentLoaded",func(){};
-//$(document).ready(func(){});
 document.addEventListener("DOMContentLoaded", function() {
     // const mq = window.matchMedia("(min-width: 500px)");
     var tabColor = document.querySelectorAll('.navigate ul li');
-    var over = document.querySelectorAll('.overlay');
-
-    var rent = document.querySelector('#rent');
-    var renter = document.querySelector('#renter');
-    var data = document.querySelector('#data');
     var ChangeTapColor = function() {
         for (let i = 0; i < tabColor.length; ++i) {
             tabColor[i].onclick = () => {
@@ -25,28 +18,8 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
     }
-    rent.onclick = () => {
-            // console.log(`${rent} clicked height is ${ renter.style.height} `);
-            if (renter.style.visibility === 'visible') {
-                renter.style.height = `${350}vh`;
-                console.log(`${renter} height is ${ renter.style.height} `);
-            } else {
-                renter.style.height = '0';
-                console.log(`${renter} height is ${ renter.style.height} `);
-            }
-        }
-        // var overlay = (x) => {
-        //     if (x.style.visibility === 'visible') {
-        //         x.style.height = `${350}vh`;
-        //         console.log(`${x} height is ${ x.style.height} `);
-        //     } else {
-        //         x.style.height = '0';
-        //         console.log(`${x} height is ${ x.style.height} `);
-        //     }
 
-    // }
-    // overlay(renter);
-    // overlay(data);
+
     ChangeTapColor();
 
 });
